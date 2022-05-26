c7ca6ea8e3e3eabfbdf78654c3843cefdb2a0cf7
Maksim <teramor1986@gmail.com>
2022-05-26 19:41:45 +0300

    some changes

--- FIFO and LIFO.py
+++ FIFO and LIFO.py
@@ -1,50 +1,27 @@
 
 def get_goods(filename):
-    with open(filename, 'r', encoding='utf-8') as f:
-        goods = f.readlines()
-        return goods
-
-def get_goods_home(filename):
-    with open(filename, 'r', encoding='utf-8') as f:
-        goods = f.readlines()
-        return goods
+    with open(filename, 'r+', encoding='utf-8') as file:
+        data = file.readlines()
+        return data
 
 STRATEGY = input('LIFO/FIFO')
-
-while STRATEGY:
-    if STRATEGY == ('FIFO'):
-        goods = get_goods_home('d:/LifoAndFifo/data.txt')
-        person_have = input('что у вас?')
-        while goods:
-            if person_have == 'exit':
-                quit()
-            if not person_have:
-                item = goods.pop()
-                i = item.split()
-                print('Возьмите, вот вам', ' '.join(i))
-            else:
-                with open('d:/LifoAndFifo/data.txt', 'a', encoding='utf-8') as f:
-                    f.writelines(['\n' + person_have])
-                    print('Спасибо')
-            person_have = input('что у вас?')
-        STRATEGY = input('LIFO/FIFO')
-
-    elif STRATEGY == ('LIFO'):
-        person_have = input('что у вас?')
-        goods = get_goods('d:/LifoAndFifo/data.txt')
-        while goods:
-            if person_have == 'exit':
-                quit()
-            if not person_have:
-                item = goods.pop()
-                i = item.split()
-                print('Возьмите, вот вам', ' '.join(i))
-            else:
-                with open('d:/LifoAndFifo/data.txt', 'a', encoding='utf-8') as f:
-                    f.writelines(['\n' + person_have])
-            person_have = input('что у вас?')
-        STRATEGY = input('LIFO/FIFO')
-    elif STRATEGY == ('exit'):
-        break
-    else:
-        STRATEGY = input('LIFO/FIFO')
+goods = get_goods('d:/LifoAndFifo/data.txt')
+person_have = input('что у вас?')
+while goods:
+    if person_have == 'exit':
+        quit()
+    if not person_have:
+        if STRATEGY == ('FIFO'):
+            item = goods.pop(0)
+            i = item.split()
+            print('Возьмите, вот вам', ' '.join(i))
+        elif STRATEGY == ('LIFO'):
+            item = goods.pop()
+            i = item.split()
+            print('Возьмите, вот вам', ' '.join(i))
+    if person_have:
+        with open('d:/LifoAndFifo/data.txt', 'a', encoding='utf-8') as f:
+            f.writelines(['\n' + person_have])
+            f.close()
+            print('Спасибо')
+    person_have = input('что у вас?')
